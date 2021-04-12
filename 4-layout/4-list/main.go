3b1788c807a6e9ffe949892362e13192e49d0163
Egon Elbre <egonelbre@gmail.com>
2021-04-12 19:57:38 +0300

    4-layout/4-list: fix comments

--- 4-layout/4-list/main.go
+++ 4-layout/4-list/main.go
@@ -5,12 +5,8 @@ package main
 import (
 	"fmt"
 
-	"gioui.org/font/gofont" // gofont is used for loading the default font.
-	"gioui.org/layout"      // layout is used for layouting widgets.
-
-	// text contains constants for text layouting.
-	// unit contains metric conversion
-	// widget contains state for different widgets
+	"gioui.org/font/gofont"     // gofont is used for loading the default font.
+	"gioui.org/layout"          // layout is used for layouting widgets.
 	"gioui.org/widget/material" // material contains material design widgets.
 
 	"github.com/golangestonia/learn-gio/qapp" // qapp contains convenience funcs for this tutorial
