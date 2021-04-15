2d02df6ee9ff4f417f87a0458b5f79f34b19447c
Egon Elbre <egonelbre@gmail.com>
2021-04-15 19:16:39 +0300

    1-drawing: separate outline and stroke

---
+++ 1-drawing/2-outline/main.go
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: Unlicense OR MIT
+
+package main
+
+import (
+	"image/color"
+
+	"gioui.org/f32"      // f32 contains float32 points.
+	"gioui.org/op"       // op is used for recording different operations.
+	"gioui.org/op/clip"  // clip contains operations for clipping painting area.
+	"gioui.org/op/paint" // paint contains operations for coloring.
+
+	"github.com/golangestonia/learn-gio/qapp" // qapp contains convenience funcs for this tutorial
+)
+
+func main() {
+	qapp.Render(func(ops *op.Ops) {
+		// create a custom clip shape
+		var p clip.Path
+		p.Begin(ops)
+		p.MoveTo(f32.Pt(30, 30))
+		p.LineTo(f32.Pt(170, 170))
+		p.LineTo(f32.Pt(80, 170))
+		// the path must be closed
+		p.Close()
+
+		// set the clip to the outline
+		clip.Outline{
+			Path: p.End(),
+		}.Op().Add(ops)
+
+		// color the clip area:
+		red := color.NRGBA{R: 0xFF, A: 0xFF}
+		paint.ColorOp{Color: red}.Add(ops)
+		paint.PaintOp{}.Add(ops)
+	})
+}
---
+++ 1-drawing/3-stroke/main.go
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: Unlicense OR MIT
+
+package main
+
+import (
+	"image/color"
+
+	"gioui.org/f32"      // f32 contains float32 points.
+	"gioui.org/op"       // op is used for recording different operations.
+	"gioui.org/op/clip"  // clip contains operations for clipping painting area.
+	"gioui.org/op/paint" // paint contains operations for coloring.
+
+	"github.com/golangestonia/learn-gio/qapp" // qapp contains convenience funcs for this tutorial
+)
+
+func main() {
+	qapp.Render(func(ops *op.Ops) {
+		var p clip.Path
+		p.Begin(ops)
+		p.MoveTo(f32.Pt(30, 30))
+		p.LineTo(f32.Pt(170, 170))
+		p.LineTo(f32.Pt(80, 170))
+		// p.Close()
+
+		// set the clip to the stroke of the path
+		clip.Stroke{
+			Path: p.End(),
+			Style: clip.StrokeStyle{
+				Width: 20,
+				Cap:   clip.RoundCap,  // clip.FlatCap, clip.SquareCap
+				Join:  clip.RoundJoin, // clip.BevelJoin
+			},
+		}.Op().Add(ops)
+
+		// color the clip area:
+		red := color.NRGBA{R: 0xFF, A: 0xFF}
+		paint.ColorOp{Color: red}.Add(ops)
+		paint.PaintOp{}.Add(ops)
+	})
+}
